d6313384c058d746ac9131af951a687a61dee1b1
Zed <z332007851@gmail.com>
2018-05-24 15:40:49 +0800

    fix: dynamic remove task

--- redisbeat/scheduler.py
+++ redisbeat/scheduler.py
@@ -95,13 +95,17 @@ class RedisScheduler(Scheduler):
         return True
 
     def remove(self, task_key):
-        self.rdb.zrem(self.key, task_key)
+        tasks = self.rdb.zrangebyscore(self.key, 0, sys.maxint, withscores=True)
+        for task, score in tasks:
+            entry = pickle.loads(task)
+            if entry.name == task_key:
+                self.rdb.zrem(self.key, task)
 
     def tick(self):
         if not self.rdb.exists(self.key):
             logger.warn("key: {} not in rdb".format(self.key))
-            for e in values(self.schedule):
-                self.rdb.zadd(self.key, self._when(e, e.is_due()[1]) or 0, pickle.dumps(e))
+           # for e in values(self.schedule):
+           #    self.rdb.zadd(self.key, self._when(e, e.is_due()[1]) or 0, pickle.dumps(e))
 
         tasks = self.rdb.zrangebyscore(
             self.key, 0,
